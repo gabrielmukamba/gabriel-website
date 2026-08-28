3d3fc11f781ecaa345ae30f673eefe63ef2c2f62
Gabriel Mukamba <gabrielmukamba7@gmail.com>
2026-08-29 02:23:29 +0300

    Add To-Do list project to portfolio

--- Code/Pgm_display_2_20.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    for (int i = 2; i <=20; i += 2){
-        printf("%d\n", i);
-    }
-    return 0;
-}        
--- Code/square_display.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    for (int i = 1; i <= 10; i++) {
-        printf("square of %d is %d\n", i, i * i);
-    }
-    return 0;
-}    
